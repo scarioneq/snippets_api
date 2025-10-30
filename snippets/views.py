0540aeca35848aa437b0b0aeb71ebaf1f6f7c60f
scarione <scarioneq@gmail.com>
2025-10-30 17:01:22 +0700

    Обновили модель снипета, добавили туда поля, которые будут использоваться для views пользователя. Создали views пользователя, добавили маршруты для юзеров. Переопределили в классе SnippetList метод, который привязывает создание снипета и пользователя. Обновили сериализатор снипета, чтобы он также включал в себя юзера, создавшего снипет.

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,26 @@
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
 
+   def perform_create(self, serializer):
+      serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
 
+class UserList(generics.ListAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
