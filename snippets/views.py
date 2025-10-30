89a757a907d619cf550bbc227f340d20bffb5dd4
scarione <scarioneq@gmail.com>
2025-10-30 17:19:54 +0700

    создали кастомное ограничение, для того, чтобы возможность удалить, обновить снипет была только у автора, а чтение доступно всем

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 
 from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions
 
@@ -16,7 +17,7 @@ class SnippetList(generics.ListCreateAPIView):
       serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
