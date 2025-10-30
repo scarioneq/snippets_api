bf8fed6a27b2362168bea79f307e2c61e5093e14
scarione <scarioneq@gmail.com>
2025-10-30 17:53:42 +0700

    Создаем единую точку входа для api (где будут отображены все наши апи маршруты, куда можно обратится). Для этого создали представление с декоратором @api_view. Создали точку входа для html представления снипетов. Создали гиперссылку для связывания юзера и снипета, обновили сериализатор.

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,21 @@
-from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
-from rest_framework import permissions
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+   return Response({
+       'users': reverse('user-list', request=request, format=format),
+       'snippets': reverse('snippet-list', request=request, format=format)
+   })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -31,3 +42,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
    queryset = User.objects.all()
    serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+   queryset = Snippet.objects.all()
+   renderer_classes = [renderers.StaticHTMLRenderer]
+
+   def get(self, request, **kwargs):
+       snippet = self.get_object()
+       return Response(snippet.highlighted)
